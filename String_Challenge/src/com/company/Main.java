129486737e29801791e5d91f56866876faf89957
Xema <100383533@alumnos.uc3m.es>
2021-06-08 16:34:04 +0200

    String Challenges Upload: Reverse Words and Digits TO DO: Review QuickSort

--- String_Challenge/src/com/company/Main.java
+++ String_Challenge/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 	// How do you print duplicate characters from a string?
         String myStr= "Test String";
-        printDuplicateChar(myStr);
+        //printDuplicateChar(myStr);
 
         // check if two strings are anagrams of each other
         myStr= "I am Lord Voldemort";
@@ -17,11 +17,20 @@ public class Main {
 
         //find all permutations of a string
         myStr="ABCD";
-        printAllPermutations(myStr);
+        //printAllPermutations(myStr);
 
         //Check if string is palindrome
         myStr="aabbaa";
-        control= checkPalindrome(myStr);
+        //control= checkPalindrome(myStr);
+
+        //Check if string contains only digits
+        myStr="17485745 ";
+        control=checkDigits(myStr);
+
+        //Reverse words in a sentence
+        myStr="Reverse words in a sentence";
+        System.out.println(myStr);
+        control=reverseWords(myStr);
     }
 
     public static void printDuplicateChar(String myStr){
@@ -100,6 +109,54 @@ public class Main {
             }
         }
         System.out.println("The String is a palindrome");
+        return 1;
+    }
+
+    public static int checkDigits(String myStr){
+        //We try to convert it to a double. If possible, we know it is a number
+        try {
+            double d = Double.parseDouble(myStr);
+        } catch (NumberFormatException nfe) {
+            System.out.println("String contains non digits");
+            return -1;
+        }
+        System.out.println("String is a number");
+        return 1;
+    }
+
+    public static int reverseWords(String myStr){
+        char [] myCharStr=myStr.toCharArray();
+        if (myCharStr.length<2){
+            System.out.println("Not enough letters to reverse");
+            return -1;
+        }
+        for (int i=0;i< myCharStr.length/2;i++) {
+            swap(myCharStr, i, myCharStr.length - 1 - i);//Swap left to right to invert the whole sentence. Similar to what was used for checking palindrome
+        }
+        System.out.print("If we want full reversion: ");
+        System.out.println(myCharStr);
+        //We now reverse individual words
+        int finishWord=0;
+        for (int i=0;i< myCharStr.length;i++){
+            //Each time we reach a blank space we reach the end of a word
+            if(myCharStr[i]==' '){
+                //We calculate the centre of the word
+                int aux=(i-finishWord)/2;
+                for (int j=finishWord;j<(i-aux);j++){
+                    //We have to add finishWord to the top limit to take into account only that segment
+                    swap(myCharStr,j, i-1-j+finishWord);
+                }
+                finishWord=i+1;
+            }
+        }
+        //We also have to reverse the last word
+        int aux=(myCharStr.length-finishWord)/2;
+        for (int j=finishWord;j<(myCharStr.length-aux);j++){
+            //We have to add finishWord to the top limit to take into account only that segment
+            swap(myCharStr,j, myCharStr.length-1-j+finishWord);
+        }
+        System.out.print("If we only want to reverse the order of words: ");
+        System.out.println(myCharStr);
         return 1;
     }
 }
